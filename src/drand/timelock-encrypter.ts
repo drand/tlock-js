eb27634f19531e6f6c9f21c784cb520d63c397b2
Patrick McClurg <patrick.mcclurg@protocol.ai>
2023-03-20 08:38:45 +0100

    g1/g2swap for mainnet

--- src/drand/timelock-encrypter.ts
+++ src/drand/timelock-encrypter.ts
@@ -1,20 +1,26 @@
-import {PointG1} from "@noble/bls12-381"
+import {PointG1, PointG2} from "@noble/bls12-381"
 import {sha256} from "@noble/hashes/sha256"
 import * as ibe from "../crypto/ibe"
 import {ChainClient} from "drand-client"
 import {Stanza} from "../age/age-encrypt-decrypt"
-import {Ciphertext} from "../crypto/ibe"
+import {Ciphertext, CiphertextOnG2} from "../crypto/ibe"
 
-export function createTimelockEncrypter(client: ChainClient, roundNumber: number) {
+export function createTimelockEncrypter(client: ChainClient, roundNumber: number, swapped = false) {
     if (roundNumber < 1) {
         throw Error("You cannot encrypt for a roundNumber less than 1 (genesis = 0)")
     }
 
     return async (fileKey: Uint8Array): Promise<Array<Stanza>> => {
         const chainInfo = await client.chain().info()
-        const point = PointG1.fromHex(chainInfo.public_key)
         const id = hashedRoundNumber(roundNumber)
-        const ciphertext = await ibe.encrypt(point, id, fileKey)
+        let ciphertext: Ciphertext | CiphertextOnG2
+        if (!swapped) {
+            const point = PointG1.fromHex(chainInfo.public_key)
+            ciphertext = await ibe.encrypt(point, id, fileKey)
+        } else {
+            const point = PointG2.fromHex(chainInfo.public_key)
+            ciphertext = await ibe.encryptToG2(point, id, fileKey)
+        }
 
         return [{
             type: "tlock",
@@ -24,12 +30,13 @@ export function createTimelockEncrypter(client: ChainClient, roundNumber: number
     }
 }
 
-function hashedRoundNumber(round: number): Uint8Array {
+
+export function hashedRoundNumber(round: number): Uint8Array {
     const roundNumberBuffer = Buffer.alloc(64 / 8)
     roundNumberBuffer.writeBigUInt64BE(BigInt(round))
     return sha256(roundNumberBuffer)
 }
 
-function serialisedCiphertext(ciphertext: Ciphertext): Uint8Array {
+function serialisedCiphertext(ciphertext: Ciphertext | CiphertextOnG2): Uint8Array {
     return Buffer.concat([ciphertext.U.toRawBytes(true), ciphertext.V, ciphertext.W])
 }
--- test/drand/integration.test.ts
+++ test/drand/integration.test.ts
@@ -1,11 +1,18 @@
 import "isomorphic-fetch"
-import {testnetClient, timelockDecrypt, timelockEncrypt} from "../../src"
+import {mainnetClient, testnetClient, timelockDecrypt, timelockEncrypt} from "../../src"
 
 describe("integration", () => {
     it("should be able to encrypt and decrypt with the testnet client", async () => {
         const message = "hello world and other things"
         const ciphertext = await timelockEncrypt(1, Buffer.from(message), testnetClient())
         const plaintext = await timelockDecrypt(ciphertext, testnetClient())
+
+        expect(plaintext).toEqual(message)
+    })
+    it("should be able to encrypt and decrypt with g1/g2 swapped client", async () => {
+        const message = "hello world and other things"
+        const ciphertext = await timelockEncrypt(1, Buffer.from(message), mainnetClient(), true)
+        const plaintext = await timelockDecrypt(ciphertext, mainnetClient(), true)
 
         expect(plaintext).toEqual(message)
     })
